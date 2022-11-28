75f8478e22e4e25c2ca4be58a23f4bb17c9426fa
Emma Greaves <104283854+emmagreaves@users.noreply.github.com>
2022-11-28 00:54:45 +0000

    Fixing to include minimums

--- app.py
+++ app.py
@@ -22,10 +22,11 @@ def calculate():
         coeff = 0.1917802797
         mincoeff= 0.02783
         minutes = round((coeff)*(x/7 + y/5 + z/7 + s/5)*60)
-        # minNumber = round((mincoeff)*(x/7 + y/5 + z/7 + s/5)*60)
-        # number = round((researchNumber + minNumber)/2)
+        minNumber = round((mincoeff)*(x/7 + y/5 + z/7 + s/5)*60)
+        minMinutes = round((researchNumber + minNumber)/2)
         hours, minutes = divmod(minutes, 60)
-        return render_template("calculated.html", hours = hours, minutes = minutes)
+        minHours, minMinutes = divmod(minMinutes, 60)
+        return render_template("calculated.html", hours = hours, minutes = minutes, minMinutes = minMinutes, minHours = minHours)
     else:
         return render_template("index.html")
 
